e7c04312a52b601c8302df7491e4f15987a97d1c
Prince Prajapati <princeprajapati8948@gmail.com>
2023-05-17 19:05:15 +0530

    ...

--- frontend/src/components/main/StudentLogin.js
+++ frontend/src/components/main/StudentLogin.js
@@ -8,10 +8,10 @@ const StudentLogin = () => {
     const studentLogin = Yup.object().shape({
         email: Yup.string().email('Invalid email').required('Email is Required'),
         password: Yup.string()
-            .required('No password provided.')
-            .max(15, 'Password is too long - should be 20 chars maximum.').required('Password is required')
-            .min(8, 'Password is too short - should be 8 chars minimum.')
-            .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
+            .required('Required')
+    //         .max(15, 'Password is too long - should be 20 chars maximum.').required('Password is required')
+    //         .min(8, 'Password is too short - should be 8 chars minimum.')
+    //         .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
     });
 
     const StudentLogin = useFormik({
